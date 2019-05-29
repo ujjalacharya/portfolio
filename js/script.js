a6537578a91814c8b5924a987a971c7066e2aded
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2019-05-29 20:06:21 +0545

    add typed sub intro

--- js/script.js
+++ js/script.js
@@ -4,4 +4,13 @@ window.addEventListener("load", () => {
     play: 5000,
     pagination: false
   });
+
+  var typed = new Typed(".typed", {
+    // Waits 1000ms after typing "First"
+    strings: ["Software Engineer", "Web Developer", "Student"],
+    typeSpeed: 70,
+    loop: true,
+    showCursor: true,
+    startDelay: 1000
+  });
 });
